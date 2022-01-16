6f01e00c1fb6e326f3ab022fdd3beb3d287b2f3c
Amaia <amaiacoimbra@gmail.com>
2022-01-16 02:51:54 -0300

    Exercicios Python

---
+++ ex016.py
@@ -0,0 +1,11 @@
+'''#import math
+from math import trunc
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num) ))'''
+
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
+
+
+
+
